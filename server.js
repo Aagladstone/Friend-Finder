aa076c3920bf6c9a9ce2f167fffe52cd3b36683e
Aaron <aarongladstone456@gmail.com>
2019-03-06 12:00:46 -0600

    getting basics done

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
- 
- 
- var express = require("express");
+
+
+var express = require("express");
  var path = require("path");
 
- 
+ var app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+var PORT = process.env.PORT || 3001;
+
+var friends = [];
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("app listening on Port: " + PORT)
+})
