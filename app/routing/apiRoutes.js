4d36cd91cfa80e46bbbf886cfea3182e26d11953
Aaron <aarongladstone456@gmail.com>
2019-03-07 21:44:17 -0600

    connect servers

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,23 +3,44 @@
 
 
 
+var friendsData = require("../data/friends");
+// var surveyData = require("../public/survey")
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-  });
-  
-  app.post("/api/characters", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newcharacter = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newcharacter);
-  
-    characters.push(newcharacter);
-  
-    res.json(newcharacter);
-  });
+module.exports = function(app) {
+
+app.get("/api/friends", function(req, res) {
+  res.json(friendsData);
+});
+
+// app.get("/api/survey", function(req, res) {
+//   res.json(surveyData);
+// })
+
+app.post("/api/friends", function(req, res) {
+  friendsData.push(req.body);
+  res.json(true);
+}); 
+
+};
+
+
+
+// app.get('/api/friends', function(req, res){
+//   res.json(friends); // sends content from tables to URL as JSON
+// });
+
+// app.post("/api/friends", function(req, res){ // NOTE: request, is to receive data. res , is to send data
+//   // Create a variable to request/store data from the form
+//   var reservation = req.body;
+
+//   // There are only 5 tables therefore we check if there are any tables avaliable and then push the data to the appropriate array
+//   if (tables.length < 5){
+//       tables.push(reservation);
+//       res.json({reservation: true}); // This creates an object with the property of reservation. Reservation has the value true or false depending on table availibility 
+//       console.log(reservation);
+//   }
+//   else{
+//       waitlist.push(reservation);
+//       res.json({reservation: false});
+//   }
+// });
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 
 
 var express = require("express");
- var path = require("path");
+var path = require("path");
 
  var app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+
 
 var PORT = process.env.PORT || 3001;
 
-var friends = [];
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// Create set of routes that display tables data as JSONs
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, "app/public/home.html"));
+// });
+
+// app.get('/survey', function(req, res) {
+//     res.sendFile(path.join(__dirname, "app/public/survey.html"));
+// });
+
+// app.get('/friends', function(req, res) {
+//     res.readFile(path.join(__dirname, "app/data/friends.js"))
+// });
 
 app.listen(PORT, function() {
-    console.log("app listening on Port: " + PORT)
+    console.log("App listening on Port: " + PORT)
 })
